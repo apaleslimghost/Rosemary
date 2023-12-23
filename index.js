854a7236b0b4fb4acf5691094d1bb2b4cfb8bd4a
Kara Brightwell <kara@153.io>
2023-12-23 10:49:51 +0000

    don't use classes at all

--- index.js
+++ index.js
@@ -4,45 +4,42 @@ const toposort = require('toposort')
 const moment = require('moment')
 const parseDuration = require('parse-duration')
 
-class Task {
-	/**
-	 * @param {string} length
-	 * @param {string[]} deps
-	 */
-	constructor(length, deps) {
-		this.length = moment.duration(parseDuration(length))
-		this.deps = deps
-	}
 
-	static deps(/** @type {Record<string, Task>} */ tasks) {
-		return Object.entries(tasks).flatMap(([name, task]) => task.deps.map(
-			/** @returns {[string, string]} */
-			dep => [name, dep]
-		))
-	}
+/** @typedef {{ length: moment.Duration, deps: string[] }} Task */
 
-	static sort(/** @type {Record<string, Task>} */ tasks) {
-		return toposort(this.deps(tasks)).map((k) => tasks[k])
-	}
+/** @returns Task */
+const task = (
+	/** @type {string} */ length,
+	/** @type {string[]} */ deps
+) => ({
+	length: moment.duration(parseDuration(length)),
+	deps
+})
 
-	static deadline(/** @type {Record<string, Task>} */ tasks, /** @type {moment.Moment} */ time) {
-		return this.sort(tasks).map((task) =>
-			 [
-				time.subtract(task.length).clone(),
-				task
-			]
-		)
-	}
+const deps = (/** @type {Record<string, Task>} */ tasks) => {
+	return Object.entries(tasks).flatMap(([name, task]) => task.deps.map(
+		/** @returns {[string, string]} */
+		dep => [name, dep]
+	))
+}
 
-	toString() {
-		return this.name + '(' + this.length.humanize() + ')'
-	}
+const sort = (/** @type {Record<string, Task>} */ tasks) => {
+	return toposort(deps(tasks)).map((k) => tasks[k])
+}
+
+const deadline = (/** @type {Record<string, Task>} */ tasks, /** @type {moment.Moment} */ time) => {
+	return sort(tasks).map((task) =>
+			[
+			time.subtract(task.length).clone(),
+			task
+		]
+	)
 }
 
 const tasks = {
-	boil: new Task('10m', ['chop']),
-	chop: new Task('5m', ['wash']),
-	wash: new Task('1m', []),
+	boil: task('10m', ['chop']),
+	chop: task('5m', ['wash']),
+	wash: task('1m', []),
 }
 
-console.log(Task.deadline(tasks, moment()))
+console.log(deadline(tasks, moment()))
