166f83b847efbb9911d2abb795ac8295960a5eaf
Kara Brightwell <kara@153.io>
2023-12-23 17:25:22 +0000

    completely rethink

--- index.js
+++ index.js
@@ -1,37 +1,53 @@
 // @ts-check
 
-const toposort = require('toposort')
 const parseDuration = require('parse-duration')
+const humanizeDuration = require('humanize-duration')
 
-/** @typedef {{ length: number, deps: string[] }} Task */
+/** @typedef {{ length: number, name: string, startTime?: Date }} Task */
 
 /** @returns Task */
 const task = (
 	/** @type {string} */ length,
-	/** @type {string[]} */ deps
+	/** @type {string} */ name
 ) => ({
 	length: parseDuration(length),
-	deps
+	name
 })
 
-const deps = (/** @type {Record<string, Task>} */ tasks) => {
-	return Object.entries(tasks).flatMap(([name, task]) => task.deps.map(
-		/** @returns {[string, string]} */
-		dep => [name, dep]
-	))
+/** @returns {Task[]} */
+const scheduleTasks = (/** @type {Task[]} */ [lastTask, ...otherTasks], /** @type {Date} */ endTime) => {
+	if(!lastTask) return []
+
+	const startTime = new Date(endTime.getTime() - lastTask.length)
+
+	return [
+		{...lastTask, startTime},
+		...scheduleTasks(otherTasks, startTime)
+	]
 }
 
-const schedule = (/** @type {Record<string, Task>} */ tasks, /** @type {Date} */ time) => {
-	return toposort(deps(tasks)).map((task) => ({
-		time: new Date(time.getTime() - tasks[task].length),
-		task
-	}))
-}
+const printTask = (/** @type {Task} */ task) => `${task.startTime?.toLocaleTimeString('en-GB')} ${task.name} (${humanizeDuration(task.length)})`
 
-const tasks = {
-	boil: task('10m', ['chop']),
-	chop: task('5m', ['wash']),
-	wash: task('1m', []),
-}
+const scheduleGroups = (/** @type {Task[][]} */ groups, /** @type {Date} */ endTime) => groups.map(group => scheduleTasks(group, endTime))
 
-console.log(schedule(tasks, new Date()).map(({ time, task }) => `${task} ${tasks[task].length} ${time.toTimeString()}`))
+const groups = [
+	[
+		task('1h', 'roast potatoes'),
+		task('10m', 'baste potatoes'),
+		task('20m', 'parboil potatoes'),
+		task('20m', 'chop potatoes')
+	],
+	[
+		task('20m', 'rest meat'),
+		task('3h', 'roast meat'),
+		task('10m', 'sear meat')
+	]
+]
+
+console.log(
+	scheduleGroups(groups, new Date())
+		.flat()
+		.sort((a, b) => (a.startTime?.getTime() ?? NaN) - (b.startTime?.getTime() ?? NaN))
+		.map(printTask)
+		.join('\n')
+)
