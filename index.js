a587ac70d0d4d36fcfa444d52e630409875b3721
Kara Brightwell <kara@153.io>
2023-12-23 10:47:03 +0000

    use an object of tasks instead of tracking instances

--- index.js
+++ index.js
@@ -5,32 +5,28 @@ const moment = require('moment')
 const parseDuration = require('parse-duration')
 
 class Task {
-	/** @type {Record<string, Task>} */
-	static instances = {}
-
 	/**
-	 * @param {string} name
 	 * @param {string} length
 	 * @param {string[]} deps
 	 */
-	constructor(name, length, deps) {
-		this.name = name
+	constructor(length, deps) {
 		this.length = moment.duration(parseDuration(length))
 		this.deps = deps
-
-		Task.instances[name] = this
 	}
 
-	static deps() {
-		return Object.values(this.instances).flatMap((task) => task.depPairs())
+	static deps(/** @type {Record<string, Task>} */ tasks) {
+		return Object.entries(tasks).flatMap(([name, task]) => task.deps.map(
+			/** @returns {[string, string]} */
+			dep => [name, dep]
+		))
 	}
 
-	static sort() {
-		return toposort(this.deps()).map((k) => Task.instances[k])
+	static sort(/** @type {Record<string, Task>} */ tasks) {
+		return toposort(this.deps(tasks)).map((k) => tasks[k])
 	}
 
-	static deadline(/** @type {moment.Moment} */ time) {
-		return this.sort().map((task) =>
+	static deadline(/** @type {Record<string, Task>} */ tasks, /** @type {moment.Moment} */ time) {
+		return this.sort(tasks).map((task) =>
 			 [
 				time.subtract(task.length).clone(),
 				task
@@ -41,15 +37,12 @@ class Task {
 	toString() {
 		return this.name + '(' + this.length.humanize() + ')'
 	}
-
-	/** @returns {[string, string][]} */
-	depPairs() {
-		return this.deps.map((dep) => [this.name, dep])
-	}
 }
 
-new Task('boil', '10m', ['chop'])
-new Task('chop', '5m', ['wash'])
-new Task('wash', '1m', [])
+const tasks = {
+	boil: new Task('10m', ['chop']),
+	chop: new Task('5m', ['wash']),
+	wash: new Task('1m', []),
+}
 
-console.log(Task.deadline(moment()))
+console.log(Task.deadline(tasks, moment()))
